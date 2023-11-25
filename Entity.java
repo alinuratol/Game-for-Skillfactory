66a9e89f2df48ba7a9d5cbf8bce4618593b4bc71
alinuratol <105600765+alinuratol@users.noreply.github.com>
2023-11-25 18:17:57 +0100

    Create Entity.java

---
+++ Entity.java
@@ -0,0 +1,22 @@
+public class Entity {
+    String name;
+    int health;
+    int strength;
+
+    public Entity(String name, int health, int strength) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+
+    public int attack() {
+        return strength;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health < 0) {
+            health = 0;
+        }
+    }
+}
